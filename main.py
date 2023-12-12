464bb17b834d1ee692aa238b8e517a4cb321ff11
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2023-12-12 20:50:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+# Import and initialize the pygame library
+import pygame
+from src.video.VideoPlayer import *
+from TTS.test import *
+import numpy as np
+import asyncio
+import simpleaudio as sa
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+videoPlayer = VideoPlayer()
+videoPlayer.play("./resources/videos/test.mp4")
+#function to call playht
+asyncio.run(AIvoice.async_main(user="USERID",key="KEY",text=["You are harry potter"],quality="faster",interactive=False,use_async=True,voice="s3://voice-cloning-zero-shot/d9ff78ba-d016-47f6-b0ef-dd630f59414e/female-cs/manifest.json"))
+# Run until the user asks to quit
+running = True
+while running:
+
+     #Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Draw video to display surface
+    videoPlayer.draw_to(screen, (screen.get_width()/4, screen.get_height()/5))
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
