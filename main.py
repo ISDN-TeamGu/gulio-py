988366f5e63a4a9757306c2836a0301a3a045667
hackerG7 <blackrc3636@gmail.com>
2023-12-20 00:39:24 +0800

    Remake video player

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 # Import and initialize the pygame library
 import pygame
-from moviepy.editor import VideoFileClip
 import os
+from src.video.VideoPlayer import VideoPlayer
+from multiprocessing import Process
 
 pygame.init()
 os.environ["DISPLAY"] = ":0"
 pygame.display.init()
-clip = VideoFileClip("resources/videos/test.mp4")
-clip.preview(fullscreen=True)
+videoPlayer = VideoPlayer()
 
-# Set up the drawing window
-screen = pygame.display.set_mode(clip.size, pygame.FULLSCREEN)
+time = 0
+loop_interval = 10
+
+videoPlayer.play("resources/videos/test.mp4")
 
 # Run until the user asks to quit
 running = True
 while running:
-
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Flip the display
-    pygame.display.flip()
-
-# Done! Time to quit.
-pygame.quit()
+    running = True
+    videoPlayer.draw()
+    pygame.time.wait(loop_interval)  # Limit the speed of the loop
+    time += loop_interval
+    if time >= 5000 and time < 5010:
+        videoPlayer.play("resources/videos/1211.mp4")
--- src/video/VideoPlayer.py
+++ src/video/VideoPlayer.py
@@ -1,32 +1,27 @@
-from pyvidplayer2 import Video
+import pygame
+import asyncio
+from pygamevideo import Video
 
 class VideoPlayer:
     def __init__(self):
-        self.current_video = None
+        self.window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.video = None
 
+    def draw(self):
+        if self.video is None:
+            return
+        # Draw video to display surface
+        print("drawing")
+        # this function should be called every frame
+        self.video.draw_to(self.window, (0, 0))
+
+        # Update pygame display
+        pygame.display.flip()
     def play(self, videoPath):
-        # Stop the current video
-        if self.current_video is not None:
-            self.current_video.stop()
-
-        # Load the video from the specified dir
-        self.current_video = Video(videoPath)
+        # Load the video from the specified path
+        self.video = Video(videoPath)
 
         # Start the video
-        self.current_video.play()
-    
-    def draw_to(self, pygame, surface, position):
-        # Draw the video to the specified surface
-        if self.current_video is not None:
-            # Quit when pygame quit
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.current_video.stop()
-            
-            # Draw the video
-            if self.current_video.draw(surface, position, force_draw=False):
-                pygame.display.update()
+        self.video.play()
 
-            # Wait for the next frame
-            pygame.time.wait(16) # around 60 fps
-
+        print("playing video: ", self.video.frame_width)
