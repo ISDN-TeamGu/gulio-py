7f945cf378095cfe5c1ab72e388ee5faa3ef4922
hackerG7 <blackrc3636@gmail.com>
2023-12-20 21:48:36 +0800

    Update rfid_write.py

--- rfid_read.py
+++ rfid_read.py
@@ -1,11 +1,21 @@
 import RPi.GPIO as GPIO
+from mfrc522 import MFRC522
 from mfrc522 import SimpleMFRC522
+from time import sleep
+from datetime import datetime
 
-render = SimpleMFRC522()
+reader = MFRC522()
 
 try:
-    id, text = render.read()
-    print(id)
-    print(text)
+    while True:
+        status, _ = reader.MFRC522_Request(reader.PICC_REQIDL)
+        if status != reader.MI_OK:
+            sleep(0.1)
+            continue
+        status, backData = reader.MFRC522_Anticoll()
+        buf = reader.MFRC522_Read(0)
+        reader.MFRC522_Request(reader.PICC_HALT)
+        if buf:
+            print(datetime.now().isoformat(), ':'.join([hex(x) for x in buf]))
 finally:
-    GPIO.cleanup()
+        GPIO.cleanup()
