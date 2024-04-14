c3b7fafe201b17a6f367d068dc111c7abaa28bab
TheanZ <151522967+TheanZ@users.noreply.github.com>
2024-04-14 14:56:34 +0800

    Add files via upload

--- src/command/CommandProcessor.py
+++ src/command/CommandProcessor.py
@@ -30,13 +30,14 @@ class CommandProcessor:
             print("Playing music:", song_name)
     def play_emoji(self, name, emotion):
         # Handle the playMusic command with its arguments
-        if name.lower() == "harry" or name.lower() == "dumbledore" or name.lower() == "ron" or name.lower() == "hermione":
+        #if name.lower() == "harry" or name.lower() == "dumbledore" or name.lower() == "ron" or name.lower() == "hermione":
+        if name == "temp":
             emoji_name = name
             emoji = emotion
             print("Playing emoji:", emoji_name)
             singleton.video_player.play("resources/videos/emojis/"+emoji_name.lower()+"/"+emoji.lower()+".mp4")
         else:
-            singleton.video_player.play("resources/videos/emojis/blinking.mp4")
+            singleton.video_player.play("resources/videos/emojis/harry/angry.mp4")
     
     def play_music(self, arguments):
         # Handle the playMusic command with its arguments
