35885bbcdaa5d658664f82976c4a794abc5a4555
TheanZ <151522967+TheanZ@users.noreply.github.com>
2024-04-14 18:22:34 +0800

    Add files via upload

--- NewServo.py
+++ NewServo.py
@@ -26,18 +26,3 @@ class Servo:
         self.pwm.ChangeDutyCycle(duty)
         time.sleep(speed)
 
-# Create a servo instance
-servo = Servo(18)
-
-# Move the servo to 0 degrees at slow speed
-servo.move(0, 0.1)
-
-# Move the servo to 90 degrees at medium speed
-servo.move(30, 0.05)
-
-# Move the servo to 180 degrees at fast speed
-servo.move(50, 0.02)
-
-# Cleanup
-servo.pwm.stop()
-GPIO.cleanup()
