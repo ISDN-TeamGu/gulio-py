c386e44ea5a0564faa6974a7f27149347460cf2c
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2024-04-15 16:55:00 +0800

    Add files via upload

--- src/motor/servoc.py
+++ src/motor/servoc.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
 from time import sleep   # Imports sleep (aka wait or pause) into the program
 GPIO.setmode(GPIO.BCM) # Sets the pin numbering system to use the physical layout
+import src.singleton as singleton
 
-class VideoPlayer:
+class Motor:
     def __init__(self):
         GPIO.setup(23,GPIO.OUT)  
         GPIO.setup(24,GPIO.OUT) 
