d8aa9a059c89e4c2ed217a0e00362830c47e90a9
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2024-05-19 17:06:02 +0800

    Update home.py

--- home.py
+++ home.py
@@ -107,7 +107,7 @@ def main_process():
         # STEP 3: Speak the response
         #final_result_text = singleton.text_to_speech_manager.process_text_stream(response_stream)
         #newstr = string_to_stream(STORY)
-        final_result_text = singleton.text_to_speech_manager.process_dialogue("[Vernon][Male][50][Default]"Vernon Dursley speaking."")
+        singleton.text_to_speech_manager.process_dialogue("""[Vernon][Male][50][Default]"Vernon Dursley speaking."""")
         singleton.video_player.display_image(image_path)
         # add the new question and answer to the list of previous questions and answers
         previous_questions_and_answers.append((new_question, final_result_text))
