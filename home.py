3b20fd804e55207b66477e799b477acd36a6c180
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2024-05-19 16:38:52 +0800

    Update home.py

--- home.py
+++ home.py
@@ -197,7 +197,7 @@ def main_process():
         #response_stream = singleton.chat_gpt_manager.get_response_stream(INSTRUCTIONS, previous_questions_and_answers, new_question)
                 
         # STEP 3: Speak the response
-        final_result_text = singleton.text_to_speech_manager.process_text_stream(response_stream)
+        #final_result_text = singleton.text_to_speech_manager.process_text_stream(response_stream)
         #newstr = string_to_stream(STORY)
         final_result_text = singleton.text_to_speech_manager.process_text_string(STORY)
         singleton.video_player.display_image(image_path)
