5489ec9bad97a10a7eb0c374cd0b26ff9d380345
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2024-05-19 17:19:25 +0800

    Update TextToSpeech.py

--- src/tts/TextToSpeech.py
+++ src/tts/TextToSpeech.py
@@ -145,24 +145,14 @@ class TextToSpeechManager:
         # Make result
         return "".join(sentences)
     def process_text_string(self, text_string):
-        sentences = []
-        temp = ""
-        content = text_string
+        
+        dialogue = self.process_dialogue(text_string)
+               
+     
+        self.speak_text(dialogue)
+                
     
-        for char in text_string:
-            if content is not None:
-                temp += content
-            # when detected full stop, question mark, exclamation mark, comma or new line, process the text
-            if(";" in temp or "!" in temp or "?" in temp or "\n" in temp):
-                # print("sentence: ", temp)
-                # If the text detected flag like [Narration] or [Character name], update current attribute
-                dialogue = self.process_dialogue(temp)
-                sentences.append(dialogue)
-                # Process text
-                self.speak_text(dialogue)
-                temp = ""
-    
-        return "".join(sentences)
+      
         
             
     # Obtain the attributes like age or name from output to customize voice acting 
