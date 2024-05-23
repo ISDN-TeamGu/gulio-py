093a8eaf1f587cedd7ca2b0374abf8854fc5be99
robbywongtc <124852550+robbywongtc@users.noreply.github.com>
2024-05-23 15:10:08 +0800

    Update home.py

--- home.py
+++ home.py
@@ -123,7 +123,8 @@ IMPORTANT REMINDER:
 • For the start of the story, please start with these 5 characters if possible: Dumbledore, Snape, Harry, Ron, Hermione
 • Also, for the emotion, use more emotions like: happy, sad, surprised, fear, disgust
 • When the story reaches a point that need me to do a decision, output the options for me to choose in this format: [options]["Go find Dumbledore for advices"][options]["Meet up with Ron and Hermione"][options]["Search for clues alone"], always output only 3 options. 
-• Always give the options line before asking for my response, never end your output without the options line
+• Always give the options line before asking for my response, never end your output without the options line 
+• Do not give the options with any characters, always give the options with this format: [options]["Go find Dumbledore for advices"][options]["Meet up with Ron and Hermione"][options]["Search for clues alone"]. 
 """
 lines = [
   """[Vernon][Male][50][Default]Vernon Dursley speaking. """,
